017499d79c6e120314e5cff41aacaa0b5a6273fe
Francisca <franciscaljubetic@gmail.com>
2022-04-08 23:50:11 -0400

    metodos corregidos

--- src/main/java/com/udemy/java/primeandpalindroms/numbers.java
+++ src/main/java/com/udemy/java/primeandpalindroms/numbers.java
@@ -10,56 +10,56 @@ public abstract class numbers {
 
     public static boolean isPalindrome(int num){
         String numElements = Integer.toString(num);
-        if(num == 0 || num == 1 ){
+        if(numElements.length() == 1){
             return false;
-        };
-        for(int i = 0; i < numElements.length(); i++) {
-            char first = numElements.charAt(i);
-            char last = numElements.charAt(numElements.length()-1-i);
-            if(first != last){
-                return false;
-            };
-        };
-        return true;
+        } else {
+            for(int i = 0; i < numElements.length(); i++) {
+                char first = numElements.charAt(i);
+                char last = numElements.charAt(numElements.length()-1-i);
+                if(first != last){
+                    return false;
+                }
+            }
+            return true;
+        }
     }
 
     public static boolean isPrime(int num) {
         if(num == 0 || num == 1 ){
             return false;
-        };
+        }
         for(int i = 2; i < num; i++) {
             if (num % i == 0) {
                 return false;
-            };
-        };
+            }
+        }
         return true;
     }
 
     public static boolean isFibonacci(int n){
-        ArrayList<Integer> fib = new ArrayList<Integer>();
+        ArrayList<Integer> fib = new ArrayList<>();
+        fib.add(0);
+        fib.add(1);
+        fib.add(1);
 
-        if (n <= 0){
-            fib.add(0);
-        };
-        if (n == 1){
-            fib.add(0);
-            fib.add(1);
-        };
-        if (n >= 2){
-            fib.add(0);
-            fib.add(1);
-            for (int i= 2; i< n+1; i++){
-                int fib_i = 0;
-                fib_i = fib.get(i-1)+ fib.get(i-2);
+        if (n < 0){
+            return false;
+        }
+        if (n == 0 || n==1 || n== 2 || n==3){
+            return true;
+        }
+        else {
+            for (int i= 3; i < n+1; i++){
+                int fib_i = fib.get(i-1)+ fib.get(i-2);
                 fib.add(fib_i);
-            };
-        };
+            }
+        }
 
-        for(int j= 0; j <n; j++){
+        for(int j= 0; j <n+1; j++){
             if(n == fib.get(j)){
                 return true;
-            };
-        };
+            }
+        }
         return false;
     }
 }
